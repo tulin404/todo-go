9dbc75cbba12112ba201bd61c4fcb81b20bed9fb
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-08-20 17:56:59 -0300

    Started cobra

--- cmd/root.go
+++ cmd/root.go
@@ -1,3 +1,26 @@
 package cmd
 
-// THIS PACKAGE IS FOR BASH, ZSH AND FISH COMPLETIONS USING COBRA AND WILL BE DONE LATER
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "todo",
+	Short: "A simple todo cli.",
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// THIS IMPLEMENTS A BASIC VERSION OF todo list
+		if len(args) < 1 {
+
+		}
+
+		fmt.Println(args[0])
+		return nil
+	},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
