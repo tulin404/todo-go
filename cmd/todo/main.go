8949c1100eb80de29c67e46a34370c5534b3747f
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-08-19 21:38:22 -0300

    Added initial possible args

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -8,5 +8,15 @@ import (
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("usage: todo <command>")
+		return
+	}
+
+	args := os.Args[1:]
+
+	switch args {
+		case "add":
+		case "remove":
+		case "done":
+		case "list":
 	}
 }
