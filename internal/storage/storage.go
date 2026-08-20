a5a5b0a29cef4d7ab70b29b8d133287c0f0dc910
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-08-20 10:27:33 -0300

    Created SaveTask func

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,1 +1,15 @@
 package storage
+
+import (
+	"encoding/json"
+	"os"
+)
+
+func SaveTask(file *os.File, task any) {
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "    ")
+
+	if err := encoder.Encode(task); err != nil {
+		panic("Fatal error: Failed to write new task on tasks.json.\n" + err.Error())
+	}
+}
