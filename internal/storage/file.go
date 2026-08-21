0839176318bae2020492f995d52ab08d6d605a96
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-08-21 08:38:41 -0300

    Defined data dir based on user's OS

--- internal/storage/file.go
+++ internal/storage/file.go
@@ -4,29 +4,60 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"runtime"
 )
 
+func userDataDir() string {
+	switch runtime.GOOS {
+	case "windows":
+		dir := os.Getenv("APPDATA")
+		if dir == "" {
+			log.Fatal("\033[1;31mFatal error:\033[0m APPDATA is not defined\n")
+		}
+
+		return dir
+
+	case "linux":
+		dir := os.Getenv("XDG_DATA_HOME")
+		if dir != "" {
+			return dir
+		}
+
+		home, err := os.UserHomeDir()
+		if err != nil {
+			log.Fatalf("\033[1;31mFatal error:\033[0m failed to get user's home directory: %v", err)
+		}
+
+		return filepath.Join(home, ".local", "share")
+
+	default:
+		home, err := os.UserHomeDir()
+		if err != nil {
+			log.Fatalf("\033[1;31mFatal error:\033[0m failed to get user's home directory: %v", err)
+		}
+
+		return home
+	}
+}
+
 func VerifyStorageFile() *os.File {
-	dirPath, err := os.UserCacheDir()
-	if err != nil {
-		log.Fatalf("Fatal error: no cache directory on system\n %v", err.Error())
+	dir := userDataDir()
+
+	dir = filepath.Join(dir, "todo-go")
+
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		log.Fatalf("\033[1;31mFatal error:\033[0m failed to create application's folder\n %v", err)
 	}
 
-	dirPath = filepath.Join(dirPath, "todo-go")
-
-	if err := os.MkdirAll(dirPath, 0755); err != nil {
-		log.Fatalf("Fatal error: failed to create application's folder\n %v", err)
-	}
-
-	filePath := filepath.Join(dirPath, "tasks.jsonl")
+	filePath := filepath.Join(dir, "tasks.jsonl")
 
 	file, err := os.OpenFile(
 		filePath,
-		os.O_CREATE|os.O_APPEND|os.O_WRONLY,
+		os.O_CREATE|os.O_APPEND|os.O_RDWR,
 		0644,
 	)
 	if err != nil {
-		log.Fatalf("Fatal error: failed to open file %s\n %v", filePath, err)
+		log.Fatalf("\033[1;31mFatal error:\033[0m failed to open file %s\n %v", filePath, err)
 	}
 
 	return file
