cc05d39514c707fdc6baac74f9197c277769f22b
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-08-31 11:45:19 -0300

    Created ListRaw

--- internal/task/list.go
+++ internal/task/list.go
@@ -7,6 +7,44 @@ import (
 	"github.com/tulin404/todo-go/internal/storage"
 )
 
+func ListRaw() ([]Task, error) {
+	file, err := storage.VerifyStorageFile()
+	if err != nil {
+		return nil, fmt.Errorf("failed to verify tasks file: %v", err)
+	}
+	defer file.Close()
+
+	scanner, err := storage.NewScanner(file)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list tasks: %v", err)
+	}
+
+	var tasks []Task
+
+	for {
+		line, err := storage.Next(scanner)
+
+		if err != nil {
+			return nil, fmt.Errorf("failed to list task: %v", err)
+		}
+
+		if line == nil {
+			break
+		}
+
+		var task Task
+
+		if err := json.Unmarshal(line, &task); err != nil {
+			fmt.Printf("\033[33mWarning:\033[0m invalid json line, skipping\n %v", err)
+			continue
+		}
+
+		tasks = append(tasks, task)
+	}
+
+	return tasks, nil
+}
+
 // 'List' lists all the tasks and can filter them
 func List(filters []string) error {
 	file, err := storage.VerifyStorageFile()
