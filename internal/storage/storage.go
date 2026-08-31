dee945e5d1b74e49bf17fc7b7f5ead215c78bd04
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-08-31 17:39:54 -0300

    Created remove func on package storage

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -5,29 +5,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // 'NewScanner' is an abstraction layer for no direct coupling between package task and direct data readers
 func NewScanner(file *os.File) (*bufio.Scanner, error) {
 	// ABSTRACTION LAYER
 	return bufio.NewScanner(file), nil
-}
-
-// 'Save' edits the tasks file and saves the received task
-func Save(task any) error {
-	file, err := VerifyStorageFile()
-	if err != nil {
-		return fmt.Errorf("failed to verify tasks file: %v", err)
-	}
-	defer file.Close()
-
-	encoder := json.NewEncoder(file)
-
-	if err := encoder.Encode(task); err != nil {
-		return fmt.Errorf("failed to write new task on tasks.json\n %v", err)
-	}
-
-	return nil
 }
 
 // 'Next' scans the next line of the scanner selected file on demand
@@ -49,19 +33,80 @@ func Next(scanner *bufio.Scanner) ([]byte, error) {
 	return line, nil
 }
 
-func Reset() error {
+// 'Save' edits the tasks file and saves the received task
+func Save(task any) error {
 	file, err := VerifyStorageFile()
 	if err != nil {
 		return fmt.Errorf("failed to verify tasks file: %v", err)
 	}
 	defer file.Close()
 
+	encoder := json.NewEncoder(file)
+
+	if err := encoder.Encode(task); err != nil {
+		return fmt.Errorf("failed to write new task on tasks.json\n %v", err)
+	}
+
+	return nil
+}
+
+// 'Remove' identifies and removes certain task by its ID
+func Remove(id string) error {
+	file, err := VerifyStorageFile()
+	if err != nil {
+		return fmt.Errorf("failed to verify tasks file: %v", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var lines []string
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		var task struct {
+			ID string `json:"id"`
+		}
+
+		if err := json.Unmarshal([]byte(line), &task); err != nil {
+			return fmt.Errorf("failed to parse json: %w", err)
+		}
+
+		if task.ID == id {
+			continue
+		}
+
+		lines = append(lines, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+        return fmt.Errorf("failed to scan tasks file: %w", err)
+    }
+
+    file.Close()
+
+    // EVEN THOUGHT THIS APPROACH IS LESS MEMORY EFFICIENT, IT REDUCES SYSTEM CALLS AND MAKES SURE THAT THE FILE WILL BE OVERWRITTEN ONLY IF SCAN SUCCEDS
+    return os.WriteFile(
+    	file.Name(),
+     	[]byte(strings.Join(lines, "\n")+"\n"),
+      	0644,
+    )
+}
+
+// 'Reset' resets the tasks file, wiping out every task
+func Reset() error {
+	file, err := VerifyStorageFile()
+	if err != nil {
+		return fmt.Errorf("failed to verify tasks file: %w", err)
+	}
+	defer file.Close()
+
 	if err := file.Truncate(0); err != nil {
-		return fmt.Errorf("failed to truncate file: %v", err)
+		return fmt.Errorf("failed to truncate file: %w", err)
 	}
 
 	if _, err := file.Seek(0, 0); err != nil {
-		return fmt.Errorf("failed to reset file pointer: %v", err)
+		return fmt.Errorf("failed to reset file pointer: %w", err)
 	}
 
 	return nil
