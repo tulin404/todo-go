5787af9bcf919659158d5315e61c94da8536ae7a
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-09-02 20:08:01 -0300

    Created FormatDue

---
+++ internal/timeutil/formatter.go
@@ -0,0 +1,8 @@
+package timeutil
+
+import "time"
+
+// 'FormatDue' returns the formatted due data and is an abstraction layer to prevent other packages from importing time directly (SoC)
+func FormatDue(due time.Time) string {
+	return due.Format("Mon, January 02, 15:04")
+}
--- internal/timeutil/parser.go
+++ internal/timeutil/parser.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// 'FromNow' receives a input (string) and returns the sum of this input and the current time
 func FromNow(input string) (time.Time, error) {
 	duration, err := time.ParseDuration(input)
 	if err != nil {
