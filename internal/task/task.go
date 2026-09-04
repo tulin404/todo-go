7b9d0e1bd56912b075eff956bcee1fdaa296fca6
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-09-04 17:05:57 -0300

    Added Notified property to Task

--- internal/task/task.go
+++ internal/task/task.go
@@ -6,10 +6,11 @@ import (
 
 // type 'Task' represents the complete task model ready for consume or insert
 type Task struct {
-	ID     string     `json:"id"`
-	Icon   string     `json:"icon"`
-	Name   string     `json:"name"`
-	Due    *time.Time `json:"due"`
-	Done   bool       `json:"done"`
-	Active bool       `json:"active"`
+	ID       string     `json:"id"`
+	Icon     string     `json:"icon"`
+	Name     string     `json:"name"`
+	Due      *time.Time `json:"due"`
+	Done     bool       `json:"done"`
+	Active   bool       `json:"active"`
+	Notified bool       `json:"notified"`
 }
