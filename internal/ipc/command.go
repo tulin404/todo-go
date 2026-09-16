1b3822fa1260060bcd150a242af52c3a449a4e90
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-09-16 15:06:10 -0300

    Command DTO updated

--- internal/ipc/command.go
+++ internal/ipc/command.go
@@ -4,7 +4,8 @@ import "time"
 
 // type 'Command' is a DTO struct for IPC between todo daemon <-> todo program
 type Command struct {
-	Command string 	   `json:"command"`
-	TaskID  string 	   `json:"task_id,omitempty"`
-	TaskDue *time.Time `json:"task_due,omitempty"`
+	Command  string 	`json:"command"`
+	TaskID   string 	`json:"task_id,omitempty"`
+	TaskDue  *time.Time `json:"task_due,omitempty"`
+	TaskName string 	`json:"task_name,omitempty"`
 }
