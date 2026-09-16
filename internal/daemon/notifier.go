68eb004da25a9eaa404750420675900f5e2e3d7b
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-09-16 15:06:57 -0300

    notify func created for notifying a task with beeep

---
+++ internal/daemon/notifier.go
@@ -0,0 +1,21 @@
+package daemon
+
+import (
+	"github.com/gen2brain/beeep"
+	"github.com/tulin404/todo-go/internal/storage"
+	"github.com/tulin404/todo-go/internal/task"
+)
+
+func notify(task task.Task) error {
+	err := beeep.Notify(task.Name, "Task", task.Icon)
+	if err != nil {
+		return err
+	}
+
+	err = storage.Notified(task.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
