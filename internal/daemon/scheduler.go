37c40bc1351e31bb69ca128be4af03b62b80097f
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-09-18 08:40:32 -0300

    Changed file name and formatted

--- internal/daemon/scheduler.go
+++ internal/daemon/scheduler.go
@@ -4,9 +4,9 @@ import "time"
 
 // 'newTimer' receives a due date and returns a time.Timer that matches it
 func newTimer(due time.Time) *time.Timer {
-    duration := time.Until(due)
+	duration := time.Until(due)
 
-    duration = max(0, duration)
+	duration = max(0, duration)
 
-    return time.NewTimer(duration)
+	return time.NewTimer(duration)
 }
