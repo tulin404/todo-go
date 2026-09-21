a59e69ce4cf602a11bef6ffc5df36f71a0a9548e
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-09-21 13:38:50 -0300

    Added task_removed case for handler

--- internal/daemon/handler.go
+++ internal/daemon/handler.go
@@ -5,15 +5,27 @@ import (
 	"github.com/tulin404/todo-go/internal/task"
 )
 
-func HandleCommand(currentTask task.Task, command ipc.Command) (*task.Task, error) {
+func HandleCommand(
+	currentTask task.Task,
+	command ipc.Command,
+) (
+	*task.Task,
+	bool,
+) {
 	switch command.Command {
 	case "task_added":
 		if command.Task.Due.Before(*currentTask.Due) {
-			return &command.Task, nil
+			return &command.Task, false
 		}
-		return nil, nil
+		return nil, false
+
+	case "task_removed":
+		if command.Task == currentTask {
+			return nil, true
+		}
+		return nil, false
 
 	default:
-		return nil, nil
+		return nil, false
 	}
 }
