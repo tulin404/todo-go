1ff5f2a57136c64c3de2607613eec20aa0ddd69d
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-09-22 17:27:01 -0300

    Commented func

--- internal/task/reset.go
+++ internal/task/reset.go
@@ -6,6 +6,8 @@ import (
 	"github.com/tulin404/todo-go/internal/storage"
 )
 
+// Good redundancy for SoC
+// 'Reset' is responsible for calling the Reset method from the storage package and treating its possible errors
 func Reset() error {
 	if err := storage.Reset(); err != nil {
 		return fmt.Errorf("failed to reset tasks file: %w", err)
